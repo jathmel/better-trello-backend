e7e29d409b303b43015c94fe277720fd0c958d33
jathmel <jathmel@gmail.com>
2018-08-08 16:26:24 -0400

    add db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_07_020240) do
+ActiveRecord::Schema.define(version: 2018_08_08_194154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "members", force: :cascade do |t|
+    t.integer "team_member_id"
+    t.integer "team_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
@@ -31,11 +38,24 @@ ActiveRecord::Schema.define(version: 2018_08_07_020240) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "team_member_projects", force: :cascade do |t|
+    t.integer "team_member_id"
+    t.integer "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "team_members", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password_digest"
     t.boolean "is_manager", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
